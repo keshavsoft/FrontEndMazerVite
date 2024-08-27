c6d535cb9d9a6581c19b644e61b2cfe06d2f865f
keshavsoft <nknnkumar@yahoo.com>
2024-08-27 17:08:10 +0530

    chat working

--- src/Js/Chat/FormLoad/AddListeners/SendButtonId/clickFunc/Entry.js
+++ src/Js/Chat/FormLoad/AddListeners/SendButtonId/clickFunc/Entry.js
@@ -1,17 +1,17 @@
 let StartFunc = () => {
-    console.log("gaaaaaaaaaaa : ");
-    jFLocalSendMessage();
+    let jVarLocalSendMessageInputId = document.getElementById('SendMessageInputId');
 
+    webSocket.send(jVarLocalSendMessageInputId.value);
+    jFLocalSendMessage({ inMessageToSend: jVarLocalSendMessageInputId.value });
 };
 
-const jFLocalSendMessage = () => {
-    let jVarLocalSendMessageInputId = document.getElementById('SendMessageInputId');
+const jFLocalSendMessage = ({ inMessageToSend }) => {
     const template = document.querySelector("#SendMessageId");
     let jVarLocalChatContentId = document.getElementById('ChatContentId');
 
     // Clone the new row and insert it into the table
     const clone = template.content.cloneNode(true);
-    clone.querySelector(".chat-message").innerHTML = jVarLocalSendMessageInputId.value;
+    clone.querySelector(".chat-message").innerHTML = inMessageToSend;
 
     jVarLocalChatContentId.appendChild(clone);
 };
