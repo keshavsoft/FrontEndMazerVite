9c2dd4717ffa5390b09f8e933f21be3bba9aa5c2
keshavsoft <nknnkumar@yahoo.com>
2024-09-17 19:12:59 +0530

    vertical working fixed

---
+++ src-Vertical/Js/pages/fixed-Form/FormLoad/AddListeners/StartFunc.js
@@ -0,0 +1,8 @@
+// import { StartFunc as StartFuncRefreshBSTableId } from "./RefreshBSTableId/EntryFile.js";
+import { StartFunc as SaveButtonId } from "./SaveButtonId/EntryFile.js";
+
+let StartFunc = () => {
+    SaveButtonId();
+};
+
+export { StartFunc };
