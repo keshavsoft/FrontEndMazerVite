e704295887abcc5aa559685b7eb1dcc61001dd5d
sharath0036 <ksharathbabu2@gmail.com>
2024-09-19 19:50:08 +0530

    fixing bugs

--- src-Factory/Js/pages/ReturnDC/StartFunc.js
+++ src-Factory/Js/pages/ReturnDC/StartFunc.js
@@ -1,13 +1,13 @@
-import { StartFunc as StartFuncShowOnDom } from "./ShowOnDom.js";
 import { StartFunc as StartFuncFormLoad } from "./FormLoad/StartFunc.js";
+// import { StartFunc as LoginModal } from "../../../assets/static/js/Common/NiceAdmin/Header/LoginModal/EntryFile.js";
 
 const StartFunc = () => {
+    StartFuncFormLoad();
+    // let jVarLocalFromAdmin = LoginModal({ inSuccessFunc: StartFuncShowOnDom });
     let jVarLocalFromAdmin = true;
 
     if (jVarLocalFromAdmin) {
-        StartFuncFormLoad();
-
-        StartFuncShowOnDom().then();
+       // StartFuncShowOnDom().then();
     };
 };
 
