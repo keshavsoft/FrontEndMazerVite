28da9ce2a7eed9611511477ed05aeb471a029002
sharath0036 <ksharathbabu2@gmail.com>
2024-10-04 19:48:27 +0530

    new htmls For reWash

---
+++ src-FromEntryVoucher/Js/pages/ReWashScan/FormLoad/DCCount/PostFetch.js
@@ -0,0 +1,20 @@
+import UrlJson from "./Url.json" with { type: "json" };
+
+let StartFunc = async () => {
+    let jVarLocalFilterString = getUrlQueryParams({ inGetKey: "VoucherRef" });
+    let jVarLocalFactoryName = localStorage.getItem("FactoryName");
+    let jVarLocalFetchUrl = `/Custom/Clients/Laundry/Factory/Process/Washing/ReWash/QrCode/RowCount/${jVarLocalFilterString}/${jVarLocalFactoryName}`;
+    let response = await fetch(jVarLocalFetchUrl);
+    let data = await response.json();
+
+    return await data;
+};
+
+let getUrlQueryParams = ({ inGetKey }) => {
+    const queryString = window.location.search;
+    const parameters = new URLSearchParams(queryString);
+    const value = parameters.get(inGetKey);
+    return value;
+};
+
+export { StartFunc };
