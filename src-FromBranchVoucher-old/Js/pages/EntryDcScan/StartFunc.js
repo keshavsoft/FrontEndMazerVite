861c190b53bcf07fc8c2ec5d92ad51921be65767
Srivasu215 <sri.vasu215@gmail.com>
2024-11-07 11:02:08 +0530

    FromBranchVoucher

--- src-FromBranchVoucher-old/Js/pages/EntryDcScan/StartFunc.js
+++
@@ -1,9 +0,0 @@
-import { StartFunc as StartFuncFormLoad } from "./FormLoad/StartFunc.js";
-import { StartFunc as StartFuncAddListeners } from "./AddListeners/StartFunc.js";
-
-const StartFunc = () => {
-    StartFuncFormLoad();
-    StartFuncAddListeners();
-};
-
-StartFunc();
--- src-FromBranchVoucher/Js/pages/StatusDCBranchFilter/FormLoad/StartFunc.js
+++
@@ -1,9 +0,0 @@
-import { StartFunc as StartFuncFromUrlParams } from "./FromUrlParams/EntryFile.js";
-import { StartFunc as StartFuncBuildBsTable } from "./BuildBsTable/EntryFile.js";
-
-const StartFunc = () => {
-    StartFuncBuildBsTable();
-    StartFuncFromUrlParams();
-};
-
-export { StartFunc };
