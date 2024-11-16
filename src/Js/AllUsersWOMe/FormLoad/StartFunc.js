936b9fac7bee68293a7b8657623719b348e7158e
Nalipi Sridhar Reddy <nalipisridharreddy@gmail.com>
2024-11-16 20:47:11 +0530

    alluserswithoutme

--- src/Js/AllUsersWOMe/FormLoad/StartFunc.js
+++ src/Js/AllUsersWOMe/FormLoad/StartFunc.js
@@ -1,7 +1,9 @@
 import { StartFunc as StartFuncAddListeners } from "./AddListeners/StartFunc.js";
+import { StartFunc as StartFuncBuildBsTable } from "./BuildBsTable/EntryFile.js";
 
 const StartFunc = () => {
     StartFuncAddListeners();
+    StartFuncBuildBsTable();
 };
 
 export { StartFunc };
